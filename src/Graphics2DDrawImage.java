5573229e5ea29715c02d02d9795e8396c1cd6124
MarcusDev <marcusmodio@gmail.com>
2016-05-07 02:19:14 -0500

    added resources for tilemap graphics

--- src/Graphics2DDrawImage.java
+++ src/Graphics2DDrawImage.java
@@ -13,7 +13,7 @@ public class Graphics2DDrawImage {
 	public static void main(String[] a) {
 	    JFrame window = new JFrame();
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    window.setBounds(0, 0, 1024, 768);
+	    window.setBounds(10, 0, 1024, 768);
 	    window.getContentPane().add(new MyCanvas());
 	    window.setVisible(true);
 	  }
