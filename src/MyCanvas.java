912a63ec3996ff6a3030ec3a239467f0da5382c2
MarcusDev <marcusmodio@gmail.com>
2016-05-06 21:30:11 -0500

    Graphics

--- src/MyCanvas.java
+++ src/MyCanvas.java
@@ -11,8 +11,8 @@ class MyCanvas extends JComponent {
   public void paint(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
 
-    Image img1 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Marcus\\workspace\\RPG-QUEST-\\src\\resources\\forest_town.png");
-    Image img2 = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Marcus\\workspace\\RPG-QUEST-\\src\\resources\\town_forest_tiles.png");
+    Image img1 = Toolkit.getDefaultToolkit().getImage("resources\\forest_town.png");
+    Image img2 = Toolkit.getDefaultToolkit().getImage("resources\\town_forest_tiles.png");
     g2.drawImage(img1, 0, 0, this);
     g2.drawImage(img2, 200, 200, this);
     g2.finalize();
