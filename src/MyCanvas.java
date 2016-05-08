0f6534e149fb0f3968f73ff3df05b1124c7d2af4
Erik <olsenerik33@gmail.com>
2016-05-08 16:22:08 -0500

    we got a render boiz

--- src/MyCanvas.java
+++ src/MyCanvas.java
@@ -7,14 +7,21 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 class MyCanvas extends JComponent {
-
-  public void paint(Graphics g) {
+	private int x;
+	private int y;
+	private Image[] pull = new Image[2];
+  public MyCanvas(int i, int j,Image[] assets) {
+	  x=i;
+	  y=j;
+	  pull=assets; 
+	}
+public void paint(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
 
     Image img1 = Toolkit.getDefaultToolkit().getImage("resources\\forest_town.png");
     Image img2 = Toolkit.getDefaultToolkit().getImage("resources\\town_forest_tiles.png");
     g2.drawImage(img1, 0, 0, this);
-    g2.drawImage(img2, 200, 200, this);
+    g2.drawImage(img2,x, y, this);
     g2.finalize();
   }
 }
